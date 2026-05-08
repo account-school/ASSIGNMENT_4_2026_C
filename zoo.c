265cdb511e3ef4e872aff92cefacb04840c22b98
account-school <nberg1@ucsc.edu>
2026-05-08 08:13:37 +0000

    second problem done

--- zoo.c
+++ zoo.c
@@ -13,12 +13,12 @@ typedef struct {
 int has_b(const char *s) {
     /* TODO: Return true (1) if the name s has the character 'b' in it. Otherwise, return false (0). 
              The strchr() function may come in handy. Use the man page to understand how it works. */
-    
+    return (strchr(s,'b') != NULL);
 }
 
 int should_keep(Animal *a) {
     /* TODO: Return true (1) if the animal meets the filtering criteria. Otherwise, return false (0). */
-    
+    return (has_b(a->name) || a->age > 15 || a->weight > 200.0);
 }
 
 int compare_animals(Animal *A, Animal *B) {
@@ -26,13 +26,52 @@ int compare_animals(Animal *A, Animal *B) {
              Return 0 if the animal pointed to by A is equal to the animal pointed to by B.
              Return a negative value if the animal pointed to by A is smaller than the animal pointed to by B.
              Else, return a positive value. */
+    
+    int comparison = strcmp(A->species,B->species);
 
+    if (comparison != 0) {
+        return comparison;
+    }
+
+    
+    int age_comparison = (A->age) - (B->age);
+
+    if (age_comparison != 0)  {
+        return age_comparison;
+    }
+
+    return strcmp(A->name,B->name);
+    
+
+    
 }
 
 void sort_animals(Animal *arr, int size) {
     /* TODO: Sort the animals stored in the array arr using Bubble Sort. 
              Your implementation must call the function compare_animals(). */
+    Animal copy;
+    for (int sortedElements = 0; sortedElements < size-1; sortedElements++) {
+        for (int n = 0; n < (size)-sortedElements-1; n++) {
+            Animal* animalLeftPtr = arr+n;
+            Animal* animalRightPtr = animalLeftPtr+1;
 
+            //copy left animal into 'copy' for swap purposes.
+            
+            copy.age = animalLeftPtr->age;
+            strcpy(copy.species,animalLeftPtr->species);
+            
+            copy.weight = animalLeftPtr->weight;
+
+            strcpy(copy.name,animalLeftPtr->name);
+
+            // if animal a > animal b, swap
+            if (compare_animals(animalLeftPtr,animalRightPtr) > 0) {
+                *animalLeftPtr = *animalRightPtr;
+                *animalRightPtr = copy;
+            }
+
+        }
+    }
 }
 
 int main() {
